f375fb22d2f506ce6a244b56f1080d846b715a39
leoaugustov <vieira.augustoleonardo@gmail.com>
2020-10-19 21:35:54 -0300

    Interface modificada para mostrar resultado da análise sintática

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import AnalisadorLexico from './analisadorLexico';
-
-const secaoTokenEncontradas = document.getElementById('secao-token-encontradas');
-secaoTokenEncontradas.style.display = 'none';
+import AnalisadorSintatico from './analisadorSintatico';
 
 document.querySelector('form').onsubmit = e => {
     e.preventDefault();
     const formulario = e.target;
     const entrada = new FormData(formulario).get('entrada');
-    const analisadorLexico = new AnalisadorLexico(entrada);
+    const analisadorSintatico = new AnalisadorSintatico(new AnalisadorLexico(entrada));
 
-    const containerTokens = document.querySelector('#container-tokens ul');
-    containerTokens.innerHTML = '';
+    const sintaticamenteCorreto = analisadorSintatico.analisar();
 
-    let token = analisadorLexico.encontrarProximaToken();
-    do {
-        const li = document.createElement('li');
-        li.classList.add('animate__animated', 'animate__fadeIn', 'mt-1');
-        li.innerHTML = `${token.lexema} <div class="badge badge-primary">
-                            ${token.tipo}
-                        </div>`;
-        
-        containerTokens.appendChild(li);
-    }while(token = analisadorLexico.encontrarProximaToken());
-
-    secaoTokenEncontradas.style.display = 'block';
+    document.getElementById("secao-resultado-analise-sintatica").innerHTML = `
+    <div class="alert alert-${sintaticamenteCorreto ? 'success': 'danger'} animate__animated animate__zoomIn" role="alert">
+        As expressões informadas estão <strong>sintaticamente ${sintaticamenteCorreto ? 'corretas': 'incorretas'}</strong>
+    </div>
+    `;
 }
